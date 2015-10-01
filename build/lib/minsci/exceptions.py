e9ee98838c2795d4a5f4316effbdb36d3f52ac02
adamancer <mansura@si.edu>
2015-09-30 23:48:23 -0400

    removed build directory

--- build/lib/minsci/exceptions.py
+++
@@ -1,8 +0,0 @@
-class DMSException(Exception):
-    """Root exception. Used only to except any error, never raised."""
-    pass
-
-
-class DMSTypeException(DMSException):
-    """I don't know what I am doing with this."""
-    pass
