a7a58fe7206d8747b6e449ad71f680a5f55820e3
adamancer <mansura@si.edu>
2015-10-02 17:41:55 -0400

    Revamped patch_mosaic to use skipped file list instead of tiles

--- minsci/test/test_mosaic.py
+++ minsci/test/test_mosaic.py
@@ -1,3 +1,10 @@
+from minsci.mosaic import selector
 from minsci.mosaic import mosey
+
+
+# Handle user input
+#selector = selector.Selector()
+#params = selector.get_job_settings()
+#selector.select(*params)
 
 mosey()
