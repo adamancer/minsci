2f3e7e2e5b575dd18df04275c84e022d8ddf597a
adamancer <mansura@si.edu>
2015-10-02 10:59:23 -0400

    Added coherence test. Adjusted column counting from grid notation.

--- minsci/test/test_mosaic.py
+++ minsci/test/test_mosaic.py
@@ -1,20 +1,3 @@
-from minsci.mosaic import Mosaic
+from minsci.mosaic import mosey
 
-mosaic = Mosaic('')
-mosaic.num_cols = 3
-
-tiles = ['abc-1408-{}.jpg'.format(i) for i in xrange(1,100)]
-print mosaic.sort_tiles(tiles)
-
-tiles = [
-    'abc_Grid[@0 0].jpg',
-    'abc_Grid[@0 1].jpg',
-    'abc_Grid[@0 2].jpg',
-    'abc_Grid[@1 0].jpg',
-    'abc_Grid[@1 1].jpg',
-    'abc_Grid[@1 2].jpg',
-    'abc_Grid[@2 0].jpg',
-    'abc_Grid[@2 1].jpg',
-    'abc_Grid[@2 2].jpg'
-]
-print mosaic.sort_tiles(tiles)
+mosey()
