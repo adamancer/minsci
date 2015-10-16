ca46f4265725dd69866d7f288bf8b80b22b229a7
adamancer <mansura@si.edu>
2015-10-16 13:18:14 -0400

    Iterated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='minsci',
-      version='0.1.4',
+      version='0.1.5',
       description='Tools for data management in Mineral Sciences at NMNH',
       classifiers = [
         'License :: OSI Approved :: MIT License',
