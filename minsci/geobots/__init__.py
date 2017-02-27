5294909217c1dde5ab67cef41314930daa9c1267
adamancer <mansura@si.edu>
2017-02-27 09:30:54 -0500

    Updated with new methods

--- minsci/geobots/__init__.py
+++ minsci/geobots/__init__.py
@@ -1,2 +1,3 @@
 from geobots import GeoNamesBot
-from containers import GeoList, NAME_TO_ABBR, ABBR_TO_NAME
+from containers import (GeoList, NAME_TO_ABBR, ABBR_TO_NAME,
+                        FROM_COUNTRY_CODE, TO_COUNTRY_CODE)
