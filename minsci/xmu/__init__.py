9ac9bd21c930584cecc7e03fdcd90496a83e3459
adamancer <mansura@si.edu>
2017-04-18 11:59:07 -0400

    Added AuditRecord to init

--- minsci/xmu/__init__.py
+++ minsci/xmu/__init__.py
@@ -4,6 +4,7 @@ from .xmu import XMu, write, FIELDS
 from .xmungo import XMungo, MongoBot
 from .fields import is_table, is_reference
 from .containers.xmurecord import XMuRecord
+from .containers.auditrecord import AuditRecord
 from .containers.bibliorecord import BiblioRecord
 from .containers.mediarecord import MediaRecord, EmbedFromEMu
 from .containers.minscirecord import MinSciRecord
--- minsci/xmu/containers/__init__.py
+++ minsci/xmu/containers/__init__.py
@@ -1,6 +1,7 @@
 """Defines module-specific containers for working with EMu data"""
 
 from .xmurecord import XMuRecord
+from .auditrecord import AuditRecord
 from .bibliorecord import BiblioRecord
 from .mediarecord import MediaRecord, EmbedFromEMu
 from .minscirecord import MinSciRecord
