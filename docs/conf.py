d13938c92e9cc68aa00a71a2d2c113ea22c2464f
adamancer <mansura@si.edu>
2017-05-18 18:38:55 -0400

    Added minsci.helpers to excluded

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = ['lxml', 'pyodbc']
+MOCK_MODULES = ['lxml', 'pyodbc', 'minsci.helpers']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
