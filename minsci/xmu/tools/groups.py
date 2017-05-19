93039a9321510878352e08a357f28d3e01c63323
adamancer <mansura@si.edu>
2017-05-18 20:56:42 -0400

    Added module check. Renamed irns_to_group param to irns.

--- minsci/xmu/tools/groups.py
+++ minsci/xmu/tools/groups.py
@@ -6,12 +6,12 @@ from ..xmu import XMu, write
 GROUPS = XMu(path=None, module='egroups')
 
 
-def write_group(module, irns_to_group, fp='group.xml', irn=None, name=None):
+def write_group(module, irns, fp='group.xml', irn=None, name=None):
     """Create EMu import for egroups based on a list of irns
 
     Args:
         module (str): the backend name of the module (ecatalogue, eparties, etc)
-        irns_to_group (list):
+        irns (list): list of irns to include in the group
         fp (str): path to write import file to
         irn (int or str): irn of existing group. Either this or name must be
             specified.
@@ -19,11 +19,13 @@ def write_group(module, irns_to_group, fp='group.xml', irn=None, name=None):
     """
 
     if irn is None and name is None:
-        raise Exception('Must provide either irn or name for the group')
+        raise ValueError('Must provide either irn or name for the group')
+    if not module in GROUPS.fields.schema.keys():
+        raise ValueError('{} is not a valid module'.format(module))
     group = GROUPS.container({
         'GroupType': 'Static',
         'Module': module,
-        'Keys_tab': irns_to_group
+        'Keys_tab': irns
     })
     if name is not None:
         group['GroupName'] = name
