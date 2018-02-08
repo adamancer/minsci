4ba8616dcf3b6738f7e2abd9a6d5ca5b79365d6b
adamancer <mansura@si.edu>
2018-02-08 13:36:50 -0500

    Added functions to name, copy, and verify files

--- minsci/xmu/containers/mediarecord.py
+++ minsci/xmu/containers/mediarecord.py
@@ -1,13 +1,17 @@
 """Subclass of XMuRecord with methods specific to emultimedia"""
 
 import os
+import re
+import shutil
 from collections import namedtuple
 from itertools import izip_longest
+
+from unidecode import unidecode
 
 from .xmurecord import XMuRecord
 from ..tools.multimedia.embedder import Embedder, EmbedField
 from ..tools.multimedia.hasher import hash_file
-from ...helpers import dedupe, format_catnums, oxford_comma, parse_catnum
+from ...helpers import dedupe, format_catnums, oxford_comma, parse_catnum, lcfirst, sort_catnums
 
 
 VALID_COLLECTIONS = [
@@ -55,10 +59,20 @@ KW_WHITELIST = [
     'Micrograph, reflected light'
 ]
 
-FORMATS = ('.cr2', '.gif', '.jpg', '.jpeg', '.png', '.tif', '.tiff')
+FORMATS = (
+    '.cr2',
+    '.dng',
+    '.gif',
+    '.jp2',
+    '.jpg',
+    '.jpeg',
+    '.png',
+    '.tif',
+    '.tiff'
+    )
 
 
-MediaFile = namedtuple('MediaFile', ['irn', 'filename', 'path', 'hash',
+MediaFile = namedtuple('MediaFile', ['irn', 'filename', 'path', 'hash', 'size',
                                      'width', 'height', 'is_image', 'row'])
 
 class MediaRecord(XMuRecord):
@@ -85,6 +99,10 @@ class MediaRecord(XMuRecord):
         self.defaults = {
             'DetSubject_tab': []
         }
+        self.whitelist = KW_WHITELIST
+        self.masks = {
+            'MulTitle': u'{name} (NMNH {catnum}) [AUTO]'
+        }
 
 
     def add_embedder(self, embedder, **kwargs):
@@ -95,6 +113,70 @@ class MediaRecord(XMuRecord):
     def add_cataloger(self, cataloger):
         """Add a Cataloger instance to the MediaRecord"""
         self.cataloger = cataloger
+
+
+    def check_filename(self, primary=True):
+        """Verifies that filename follows best practices"""
+        media = [self.get_primary()] if primary else self.get_all_media()
+        for mm in media:
+            stem, ext = os.path.splitext(mm.filename)
+            matches = re.findall(r'[^a-zA-Z0-9_\-]', stem)
+            if matches or ext != ext.lower():
+                return False
+        return True
+
+
+    def fix_filename(self, fn=None):
+        """Fixes filename to conform with best practices"""
+        if fn is None:
+            fn = self.get_primary().filename
+        stem, ext = os.path.splitext(fn)
+        stem = stem.replace('-', '_')
+        stem = re.sub(r'\((\d+)\)', r'_\1_', stem)
+        stem = re.sub(r'[\s_]+', u'_', unidecode(stem))
+        stem = re.sub(r'[^a-zA-Z0-9_]', '', stem)
+        print fn, '=>', stem.rstrip('_') + ext.lower()
+        return stem.rstrip('_') + ext.lower()
+
+
+    def _get_params(self, for_filename=False):
+        # Format catalog numbers
+        catnums = sort_catnums(self.catnums)
+        if len(catnums) > 1:
+            mask = '{} and others' if for_filename else '{} and others'
+            catnum = mask.format(catnums[0])
+        else:
+            catnum = catnums[0]
+        params = {
+            'catnum': catnum,
+            'catnum_simple': catnum.replace('NMNH ', '') \
+                                   .replace('USNM', '') \
+                                   .replace('-00', ''),
+            'name': self.object.object['xname'],
+            'primary': self.object.object['xname'].split(' with ')[0],
+        }
+        if for_filename:
+            return {k: v.replace(' ', '_') for k, v in params.iteritems()}
+        return params
+
+
+    def set_filename(self, mask):
+        params = self._get_params(for_filename=True)
+        ext = os.path.splitext(self('Multimedia'))[1]
+        self['MulIdentifier'] = mask.format(**params) + ext
+
+
+    def set_mask(self, key, mask):
+        assert isinstance(mask, unicode)
+        self.masks[key] = mask
+
+
+    def set_default(self, key):
+        defaults = {
+            'DetSource': self.embedder.source,
+            'DetRights': self.embedder.rights
+        }
+        self[key] = defaults[key]
 
 
     def get_all_media(self):
@@ -108,12 +190,14 @@ class MediaRecord(XMuRecord):
         if not filename:
             filename = os.path.basename(self('Multimedia'))
         is_image = filename.lower().endswith(FORMATS)
+        size = self('ChaFileSize')
         width = self('ChaImageWidth')
         height = self('ChaImageHeight')
         return MediaFile(self('irn'),
                          filename,
                          self('Multimedia'),
                          self('ChaMd5Sum'),
+                         int(size) if is_image and size else None,
                          int(width) if is_image and width else None,
                          int(height) if is_image and height else None,
                          is_image,
@@ -125,12 +209,13 @@ class MediaRecord(XMuRecord):
         paths = self('Supplementary_tab')
         files = self('SupIdentifier_tab')
         hashes = self('SupMD5Checksum_tab')
+        sizes = self('SupFileSize_tab')
         widths = self('SupWidth_tab')
         heights = self('SupWidth_tab')
-        supp_files = izip_longest(paths, files, hashes, widths, heights)
+        supp_files = izip_longest(paths, files, hashes, sizes, widths, heights)
         supplementary = []
         for i, supp_file in enumerate(supp_files):
-            path, filename, hexhash, w, h = supp_file
+            path, filename, hexhash, s, w, h = supp_file
             if not filename:
                 filename = os.path.basename(path)
             is_image = filename.lower().endswith(FORMATS)
@@ -138,6 +223,7 @@ class MediaRecord(XMuRecord):
                                            filename,
                                            path,
                                            hexhash,
+                                           int(s) if is_image and s else None,
                                            int(w) if is_image and w else None,
                                            int(h) if is_image and h else None,
                                            is_image,
@@ -157,16 +243,54 @@ class MediaRecord(XMuRecord):
                                       'AdmGUIDValue_tab')
 
 
-    def embed_metadata(self, verify_master=True):
-        """Update metadata in primary and supplementary images"""
+    def copy_to(self, path, overwrite=False, verify_image=False):
+        """Copies the primary file to a new location
+
+        Args:
+            path (str): the directory to copy the image to
+            overwrite (bool): specifies whether to overwrite existing file
+            verify_master (bool): specifies whether to verify copied file
+        """
+        primary = self.get_primary()
+        try:
+            os.makedirs(path)
+        except OSError:
+            pass
+        dst = os.path.join(path, primary.filename)
+        try:
+            open(dst, 'rb')
+        except IOError:
+            print 'Copying {} to {}...'.format(primary.path, dst)
+            shutil.copy2(primary.path, dst)
+        else:
+            if overwrite:
+                print 'Copying {} to {}...'.format(primary.path, dst)
+                os.remove(dst)
+                shutil.copy2(primary.path, dst)
+        # Verify the copy if required
+        if verify_image and hash_file(dst) != primary.hash:
+            raise ValueError('Checksums do not match')
+        self['Multimedia'] = dst
+
+
+    def embed_metadata(self, verify_image=True):
+        """Updates metadata in the primary and supplementary images"""
         rec = self.clone(self)
         for media in self.get_all_media():
             # Embed metadata or add a placeholder for non-image files
             fp = media.path
             if media.is_image:
-                if verify_master:
-                    self._verify_master(media)
-                fp = self.embedder.embed_metadata(media.path, self)
+                if verify_image and rec('irn'):
+                    self.verify_master(media)
+                # Rename file based on MulIdentifier if that is different
+                # from the filename in Multimedia
+                if media.row is None:
+                    new_name = rec('MulIdentifier')
+                else:
+                    new_name = rec('SupIdentifier_tab')[media.row - 1]
+                if fp.endswith(new_name):
+                    new_name = None
+                fp = self.embedder.embed_metadata(self, fp, new_name)
             if fp and media.row is None:
                 rec['Multimedia'] = fp
             elif media.row and rec('irn'):
@@ -174,25 +298,85 @@ class MediaRecord(XMuRecord):
                 if len(rec['Supplementary_tab({}=)']) != media.row:
                     raise ValueError
             else:
-                rec['Supplementary_tab'][media.row - 1] = fp
+                try:
+                    rec['Supplementary_tab'][media.row - 1] = fp
+                except:
+                    print fp
+                    print rec['Supplementary_tab']
+                    print media.row
+                    print media
+                    raise
+
         if rec:
             rec['irn'] = media.irn
             return rec.strip_derived().expand()
 
 
-    @staticmethod
-    def _verify_master(media):
-        """Check if download is master by comparing hashes"""
-        if hash_file(media.path) != media.hash:
+    def verify_master(self, media=None):
+        """Verifies download/copy of master file by comparing hashes"""
+        if media is None:
+            media = self.get_primary()
+        verified = hash_file(media.path) == media.hash
+        if not verified:
             raise ValueError('Checksums do not match')
+        return verified
 
 
-    def match(self):
+    def verify_import(self, images, strict=True, test=False):
+        """Verifies import against path"""
+        Image = namedtuple('Image', ['path', 'hash'])
+        for mm in self.get_all_media():
+            matches = images.get(mm.filename, [])
+            # Get MD5 hashes and store them for future use
+            hashes = {}
+            if strict:
+                for i, im in enumerate(matches):
+                    try:
+                        im.hash
+                    except AttributeError:
+                        try:
+                            matches[i] = Image(im, hash_file(im))
+                        except IOError:
+                            print 'File not found: {}'.format(im)
+                images[mm.filename] = matches
+                hashes = {im.hash: im.path for im in matches}
+            # Delete if the filename and hash match (strict) or if
+            # the filename exists (not strict)
+            ok_to_delete = ((strict and mm.hash in hashes)
+                            or (not strict and len(matches) == 1))
+            if ok_to_delete:
+                fp = hashes[mm.hash] if mm.hash in hashes else matches[0]
+            if ok_to_delete and test:
+                print 'Would delete: {}'.format(fp)
+            elif ok_to_delete:
+                print 'Deleting {}...'.format(fp)
+                #os.unlink(paths[0])
+            elif strict and mm.hash not in hashes:
+                print 'Hash mismatch: {}'.format(mm.filename)
+            elif not strict and len(matches) != 1:
+                print 'Non-unique match (n={}): {}'.format(len(matches), fp)
+            elif not matches:
+                print 'File error: No matches found for {}'.format(mm.filename)
+            else:
+                print 'Unknown error: {}'.format(mm.filename)
+            # Provide additional info about hashes if strict
+            if strict:
+                print ' File hash:\n      {}'.format(mm.hash)
+                print ' Ref hashes:'
+                for i, md5 in enumerate(sorted(hashes)):
+                    asterisk = ''
+                    if md5 == mm.hash:
+                        asterisk = '*'
+                    print '  {: >2d}. {}{}'.format(i + 1, md5, asterisk)
+                print '-' * 60
+
+
+    def match(self, ignore_suffix=False):
         """Returns list of catalog objects matching data in MulTitle"""
         parsed = parse_catnum(self('MulTitle'))
         records = []
         for identifier in parsed:
-            matches = self.cataloger.get(identifier, [])
+            matches = self.cataloger.get(identifier, [], ignore_suffix)
             for match in matches:
                 if not match in records:
                     records.append(match)
@@ -213,7 +397,8 @@ class MediaRecord(XMuRecord):
 
     def match_and_fill(self, strict=True):
         """Updates record if unique match in catalog found"""
-        print 'Matching against {}...'.format(self('MulTitle'))
+        print 'Matching on identifiers in "{}"...'.format(self('MulTitle'))
+        self.expand()
         try:
             match = self.match_one()
         except ValueError:
@@ -222,6 +407,8 @@ class MediaRecord(XMuRecord):
         else:
             print 'Unique match found! Updating record...'
             enhanced = self.clone(self)
+            enhanced.whitelist = self.whitelist
+            enhanced.masks = self.masks
             enhanced.object = match
             enhanced.catnums = self.catnums
             for key, func in enhanced.smart_functions.iteritems():
@@ -232,8 +419,10 @@ class MediaRecord(XMuRecord):
                 enhanced['DetRights'] = ('One or more objects depicted in this'
                                          ' image are not owned by the'
                                          ' Smithsonian Institution.')
+            enhanced['DetRelation_tab'] = [rel.replace('(0/', '(1/') for rel
+                                           in enhanced['DetRelation_tab']]
             enhanced['_Objects'] = [match]
-            return enhanced
+            return enhanced.expand()
 
 
     def strip_derived(self):
@@ -267,9 +456,8 @@ class MediaRecord(XMuRecord):
                 or title.endswith('[AUTO]')):
             # Use the catnum originally parsed from the title, not the one
             # from the linked record
-            xname = self.object.object['xname']
-            catnum = self.catnums[0]
-            title = u'{} ({}) [AUTO]'.format(xname, catnum).replace(' ()', ' ')
+            params = self._get_params()
+            title = self.masks['MulTitle'].format(**params).replace(' ()', ' ')
         return title
 
 
@@ -284,7 +472,7 @@ class MediaRecord(XMuRecord):
     def smart_keywords(self, whitelist=None):
         """Derives keywords from catalog"""
         if whitelist is None:
-            whitelist = KW_WHITELIST
+            whitelist = self.whitelist
         keywords = self.object.keywords
         keywords.extend([kw for kw in self('DetSubject_tab')
                          if ':' in kw or kw in whitelist])
@@ -316,23 +504,24 @@ class MediaRecord(XMuRecord):
             collections.append(coll)
         # Check if there are any non-SI objects in photos. Different
         # collections and restrictions are applied for these photos.
-        si_object = 'Collections objects (Mineral Sciences)'
-        non_si_object = 'Non-collections object (Mineral Sciences)'
-        if self.object.object['status'] != 'active':
-            rights = ('One or more objects depicted in this image are not'
-                      ' owned by the Smithsonian Institution.')
-            collections.append(non_si_object)
-            try:
-                collections.remove(si_object)
-            except ValueError:
-                pass
-            self['DetRights'] = rights
-        else:
-            collections.append(si_object)
-            try:
-                collections.remove(non_si_object)
-            except ValueError:
-                pass
+        if self('DetResourceType') == 'Specimen/Object':
+            si_object = 'Collections objects (Mineral Sciences)'
+            non_si_object = 'Non-collections object (Mineral Sciences)'
+            if self.object.object['status'] != 'active':
+                rights = ('One or more objects depicted in this image are not'
+                          ' owned by the Smithsonian Institution.')
+                collections.append(non_si_object)
+                try:
+                    collections.remove(si_object)
+                except ValueError:
+                    pass
+                self['DetRights'] = rights
+            else:
+                collections.append(si_object)
+                try:
+                    collections.remove(non_si_object)
+                except ValueError:
+                    pass
         return dedupe(collections, False)
 
 
@@ -360,6 +549,8 @@ class EmbedFromEMu(Embedder):
         self.rights = ('This image was obtained from the Smithsonian'
                        ' Institution. Its contents may be protected by'
                        ' international copyright laws.')
+        self.source = 'NMNH-Smithsonian Institution'
+        self.job_id = None
         # Use artwork identifiers to store specimen info
         object_metadata = {
             'object_number': EmbedField('ArtworkSourceInventoryNo', 16,
@@ -372,6 +563,11 @@ class EmbedFromEMu(Embedder):
                                        self.get_object_titles)
         }
         self.metadata_fields.update(object_metadata)
+
+
+    def set_job_id(self, job_id):
+        """Sets job id manually for images not imported into EMu yet"""
+        self.job_id = job_id
 
 
     @staticmethod
@@ -401,7 +597,8 @@ class EmbedFromEMu(Embedder):
         """Placeholder function returning the creator"""
         if not rec('MulCreator_tab'):
             return self.creator
-        return oxford_comma(rec('MulCreator_tab'), False)
+        return rec('MulCreator_tab')
+        #return oxford_comma(rec('MulCreator_tab'), False)
 
 
     def get_credit_line(self, rec):
@@ -410,12 +607,12 @@ class EmbedFromEMu(Embedder):
             creator = self.creator
         else:
             creator = rec('MulCreator_tab')[0]
-        return u'{}, SI'.format(creator)
+        return u'{}'.format(creator)
 
 
     def get_date_created(self, rec):
         """Placeholder function returning the date created"""
-        return self.get_mtime(rec('Multimedia'), '%Y%d%m')
+        return self.get_mtime(rec('Multimedia'), '%Y%m%d')
 
 
     def get_datetime_created(self, rec):
@@ -439,12 +636,15 @@ class EmbedFromEMu(Embedder):
 
     def get_inventory_numbers(self, rec):
         """Returns a list of catalog numbers"""
-        return self.get_objects('MulTitle')
+        return self.get_objects()
 
 
     def get_job_id(self, rec):
         """Returns the import identifier"""
-        return rec('AdmImportIdentifier')
+        job_id = rec('AdmImportIdentifier')
+        if not job_id:
+            job_id = self.job_id
+        return job_id
 
 
     def get_keywords(self, rec):
@@ -472,10 +672,10 @@ class EmbedFromEMu(Embedder):
 
     def get_special_instructions(self, rec):
         """Returns long credit line for special instructions"""
-        creator = self.get_creator(rec)
-        if creator.startswith('Unknown'):
-            creator = creator[0].lower() + creator[1:]
-        credit = ['Full credit line: Photo by {}, '
+        creators = [c if not c.startswith('Unknown') else lcfirst(c)
+                    for c in self.get_creator(rec)]
+        creator = oxford_comma(creators, False)
+        credit = ['Full credit line: Photo by {} provided courtesy of the '
                   'Smithsonian Institution'.format(creator)]
         # Add any photo enhancements logged in EMu
         contributors = [contrib for contrib in rec('DetContributor_tab')
@@ -510,7 +710,7 @@ class EmbedFromEMu(Embedder):
         return obj_data
 
 
-    def get_object_sources(self, rec, source='National Museum of Natural History'):
+    def get_object_sources(self, rec, source='NMNH-Smithsonian Institution'):
         """Returns list with museum name"""
         return [source] * len(rec.get('_Objects', []))
 
