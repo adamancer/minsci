3c968f09ce266ebf3ad7c509eb02508d72bf8222
adamancer <mansura@si.edu>
2018-05-09 15:25:12 -0400

    meteorites method now counts Antarctics

--- minsci/portal/reports.py
+++ minsci/portal/reports.py
@@ -8,14 +8,25 @@ def meteorites():
     records = get_all(collection='meteorites')
     for rec in records:
         name = rec['catalogNumber'].split('|')[0].rsplit(',', 1)[0].strip()
-        names.setdefault(name, []).append(rec)
+        names.setdefault(name, []).append(rec.get('higherGeography', ''))
     fn = filename('meteorites')
+    antarctics = {}
     with open(fn, 'wb') as f:
         writer = csv.writer(f, dialect='excel-tab')
-        writer.writerow(['Name', 'Count'])
-        for row in [(name, len(names[name])) for name in sorted(names)]:
+        writer.writerow(['Name', 'Count', 'Antarctic'])
+        for name in sorted(names):
+            count = len(names[name])
+            antarctic = 'x' if names[name][0].startswith('Antarctica') else ''
+            if antarctic:
+                antarctics[name] = len(names[name])
+            row = [name, count, antarctic]
             writer.writerow([u'{}'.format(s).encode('utf-8') for s in row])
     encode_for_excel(fn)
+    # Report total meteorites found
     print 'Found {:,} total meteorites ({:,} distinct)'.format(len(records),
                                                                len(names))
+    # Report total Antarctic meteorites found
+    num_antarctics = sum(antarctics.values())
+    print 'Found {:,} Antarctic meteorites ({:,} distinct)'.format(num_antarctics,
+                                                                   len(antarctics))
     print 'Results saved as {}'.format(fn)
