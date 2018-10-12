597b06ca78b09452b5f167161e50bf7347e352c0
adamancer <mansura@si.edu>
2018-10-12 12:33:23 -0400

    Superseded by enhancers/sites

--- minsci/geobots/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Provides tools to interact with various georeferencing webservices"""
-
-print 'Initializing geobots submodule...'
-
-from .geobots import GeoNamesBot
-from .containers import (GeoList, NAME_TO_ABBR, ABBR_TO_NAME,
-                         FROM_COUNTRY_CODE, TO_COUNTRY_CODE)
