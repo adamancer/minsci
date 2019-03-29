feea051c0dc5b33b71b98949ecde6cf4cd1ae96a
adamancer <mansura@si.edu>
2019-03-29 10:27:13 -0700

    Configured module-level logging

--- minsci/__init__.py
+++ minsci/__init__.py
@@ -1,2 +1,9 @@
 """Provides helper functions used throughout the minsci module"""
 from __future__ import unicode_literals
+
+import logging.config
+import os
+import yaml
+
+with open(os.path.join(os.path.dirname(__file__), 'logging.yml')) as f:
+    logging.config.dictConfig(yaml.safe_load(f.read()))
